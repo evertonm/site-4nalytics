e12cc1d025339eaff4a361f9858f580cdce10159
= <=>
2021-04-25 18:19:51 -0300

    add bootstrap

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,14 +1,21 @@
 import { Header } from '../components/Header';
-import { SimpleDescription } from '../components/SimpleDescription';
-import { Skills } from '../components/Skills';
+import styles from '../styles/pages/Contact.module.css';
+
 
 export default function Contact() {
     return (
         <>
             <Header />
-            <main>
-                
+            <main className={styles.contactContainer}>
+                <div className="row">
+                    <div className="col">
+                        <input type="text" className="form-control" placeholder="First name" aria-label="First name" />
+                    </div>
+                    <div className="col">
+                        <input type="text" className="form-control" placeholder="Last name" aria-label="Last name" />
+                    </div>
+                </div>
             </main>
         </>
     );
-} 
+}
