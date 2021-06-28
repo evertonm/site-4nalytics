13e835f12ffa175ddc09870e863018a64b3e7f43
= <=>
2021-06-27 23:10:58 -0300

    Change layout of contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -6,50 +6,58 @@ import emailjs from 'emailjs-com';
 
 
 export default function Contact() {
-
+    const [sendingMail, setSendingMail] = useState(false);
     const handleSubmit = (e) => {
-        e.preventDefault()        
-        if(localStorage.getItem('nextSendContact')) {
-            if(new Date().getTime() < Number(localStorage.getItem('nextSendContact'))) {
+        e.preventDefault();
+        if (localStorage.getItem('nextSendContact')) {
+            if (new Date().getTime() < Number(localStorage.getItem('nextSendContact'))) {
                 alert('Você já enviou uma mensagem recentemente. Espere um pouco :)')
                 return;
             }
         }
+        setSendingMail(true);
         const currentDate = new Date();
         currentDate.setMinutes(currentDate.getMinutes() + 3);
-        
+
         localStorage.setItem('nextSendContact', currentDate.getTime().toString());
-        emailjs.sendForm("service_suup0lk","template_jfk3p2t", e.target, 'user_Jge83BEXxhhDUKdUDAYmH')
-        .then((result) => {
-            alert('Mensagem enviada com sucesso!');
-           
-            console.log(result.text);
-        }, (error) => {
-            localStorage.removeItem('nextSendContact');
-            alert('Ops! Não foi possível enviar a mensagem. Tente novamente.')
-            console.log(error.text);
-        });       
+        emailjs.sendForm("service_suup0lk", "template_jfk3p2t", e.target, 'user_Jge83BEXxhhDUKdUDAYmH')
+            .then((result) => {
+                setSendingMail(false);
+                alert('Mensagem enviada com sucesso!');
+
+                console.log(result.text);
+            }, (error) => {
+                setSendingMail(false);
+                localStorage.removeItem('nextSendContact');
+                alert('Ops! Não foi possível enviar a mensagem. Tente novamente.')
+                console.log(error.text);
+            });
     }
 
     return (
         <>
             <Header />
-            <div className={styles.container}>
-                <form className={styles.main} onSubmit={handleSubmit}>                   
-                    <div className={styles.inputGroup} >
-                        <label htmlFor='name'>Nome</label>
-                        <input type='text' name='name' className={styles.inputField} />
+            <div className={styles.formContainer}>
+                <form className={styles.form} onSubmit={handleSubmit}>
+                    <div className={styles.title}>Bem-vindo!</div>
+                    <div className={styles.subtitle}>Entre em contato com nossa equipe</div>
+                    <div className={`${styles.inputContainer} ${styles.ic1} `}>
+                        <input id="name" className={styles.input} type="text" placeholder=" " />
+                        <div className={styles.cut}></div>
+                        <label htmlFor="name" className={styles.placeholder}>Nome</label>
                     </div>
-                    <div className={styles.inputGroup} >
-                        <label htmlFor='email'>E-mail</label>
-                        <input type='email' name='email' className={styles.inputField} />
+                    <div className={`${styles.inputContainer}`}>
+                        <input id="email" className={styles.input} type="email" placeholder=" " />
+                        <div className={styles.cut}></div>
+                        <label htmlFor="email" className={styles.placeholder}>E-mail</label>
                     </div>
-                    <div className={styles.inputGroup} >
-                        <label htmlFor='message'>Mensagem</label>
-                        <textarea name='message' className={styles.inputFieldTextArea} rows={5} cols={5}/>
+                    <div className={`${styles.textarea} ${styles.inputContainer}`}>
+                        <textarea id="message" className={`${styles.input}`}  placeholder=" " rows={5} cols={5}/>
+                        <div className={`${styles.cut} ${styles.cutShort}`}></div>
+                        <label htmlFor="message" className={styles.placeholder}>Mensagem</label>
                     </div>
-                    <button type="submit" className="btn btn-primary">Enviar e-mail</button>
-                </form >
+                    <button type="submit" className={styles.submit} disabled={sendingMail}>{!sendingMail ? 'Enviar e-mail' : 'Enviando email...'}</button>
+                </form>
             </div>
         </>
     );
